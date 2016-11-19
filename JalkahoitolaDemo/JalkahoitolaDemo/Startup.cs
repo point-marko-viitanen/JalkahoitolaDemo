241f9b67394e7e1c155eb9f00dced420a1b75d3c
Marko Viitanen <marko.viitanen@stud.pointcollege.fi>
2016-11-19 18:33:11 +0200

    Perutettu uusi MVC App

---
+++ JalkahoitolaDemo/JalkahoitolaDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JalkahoitolaDemo.Startup))]
+namespace JalkahoitolaDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
